b217e4d292dfd4f5d6417d109765308ec3b2dc88
AbdulwahabMacSourceTree <dev.abdulwahab@gmail.com>
2022-08-19 15:08:35 +0000

    edit start-test.sh

--- start-test.sh
+++ start-test.sh
@@ -9,7 +9,7 @@ if [ $ExitCodeResult -eq 0 ]
 then
   echo "ExitCodeResult -eq 0"
   gh auth status
-  gh pr  create --base MrSalman333 --head AbdulwahabDev:base-python-Abdulwahab
+  gh pr  create --base MrSalman333 --head AbdulwahabDev:base-python-Abdulwahab --title "Add Pull Request from gh CLI" --body "this is body "
   echo "ExitCodeResult -eq 0"
 else
   echo " ExitCodeResult -eq 1"
